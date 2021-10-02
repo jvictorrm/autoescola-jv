cb23254611e12d976b6a4487f20e5d0489515517
Joao Victor Moreira <jvrodriguesmoreira@gmail.com>
2021-10-02 17:55:54 -0300

    Add Welcome.stories

---
+++ src/stories/0-Welcome.stories.js
@@ -0,0 +1,15 @@
+export default {
+  title: "Introduction/Welcome",
+};
+
+export const toAutoEscolaJV = () => (
+  <div>
+    <h1>Auto Escola JV</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic nostrum
+      voluptatibus quod rerum recusandae. Doloribus numquam quasi, reprehenderit
+      officiis quia, omnis necessitatibus recusandae laudantium repudiandae
+      incidunt minima corrupti nihil placeat.
+    </p>
+  </div>
+);
