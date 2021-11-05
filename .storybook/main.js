fb7c720fdaa64355ef6b2201489cc884e0bdd8c9
Joao Victor Moreira <jvrodriguesmoreira@gmail.com>
2021-11-04 21:58:11 -0300

    Reorder Storybook Addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,16 +5,16 @@ module.exports = {
     "../src/stories/**/*.stories.@(js|jsx|ts|tsx|mdx)",
   ],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-viewport",
     {
       name: "@storybook/addon-docs",
       options: {
         configureJSX: true,
       },
     },
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-viewport",
     "@storybook/addon-knobs",
   ],
 };
